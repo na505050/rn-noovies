ed18ba589fafd825e4a0d3d0ad6bcbfd9de508b4
hohyoung <gabozanet@gmail.com>
2022-06-12 22:57:17 +0900

    Movies Screen part Two
    $ expo install expo-blur

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,30 +1,50 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import React from "react";
-import { Dimensions } from "react-native";
+import { isLoading } from "expo-font";
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, Dimensions, StyleSheet } from "react-native";
 import Swiper from 'react-native-web-swiper';
 import styled from "styled-components/native";
+import { BlurView } from "expo-blur";
+import { makeImgPath } from "../utils";
 
 const API_KEY = "5dce6d21a6dda399669ed4fb16a2ee80";
 
-
-
 const Container = styled.ScrollView`
-    background-color: ${(props) => props.theme.mainBgColor};
 `;
 const View = styled.View`
     flex: 1;    
 `;
+const Loader = styled.View`
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+`;
 
+const BgImg = styled.Image``;
+
+const Title = styled.Text``;
 
 // const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 const SCREEN_HEIGHT = Dimensions.get("window").height;
 
 const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
-    const getNowPlaying = () => {
-        fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=KR`);
-    };
 
-    return (
+    const [isLoading, setLoading] = useState(true);
+    const [nowPlaying, setNowPlaying] = useState([]);
+    const getNowPlaying = async () => {
+        const { results } = await (await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=KR`)).json();
+        setNowPlaying(results);
+        setLoading(false);
+    };
+    useEffect(() => {
+        getNowPlaying();
+    }, [])
+
+    return isLoading ? (
+        <Loader>
+            <ActivityIndicator size="large" />
+        </Loader>
+    ) : (
         <Container>
             <Swiper
                 loop
@@ -32,10 +52,17 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
                 controlsEnabled={false}
                 containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4 }}
             >
-                <View style={{ backgroundColor: "red" }}></View>
-                <View style={{ backgroundColor: "blue" }}></View>
-                <View style={{ backgroundColor: "red" }}></View>
-                <View style={{ backgroundColor: "blue" }}></View>
+                {nowPlaying.map((movie) => (
+                    <View key={movie.id}>
+                        <BgImg
+                            style={StyleSheet.absoluteFill}
+                            source={{ uri: makeImgPath(movie.backdrop_path) }}
+                        />
+                        <BlurView intensity={80} style={StyleSheet.absoluteFill}>
+                            <Title>{movie.original_title}</Title>
+                        </BlurView>
+                    </View>
+                ))}
             </Swiper>
 
         </Container>
