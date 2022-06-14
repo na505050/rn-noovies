f70e65aff72b0b0e4d230402f2e1f717aa5f973f
hohyoung <gabozanet@gmail.com>
2022-06-14 21:44:39 +0900

    Coming Soon
    날짜포맷 적용
    
    build.gradle
    // def jscFlavor = 'org.webkit:android-jsc:+'
    def jscFlavor = 'org.webkit:android-jsc-intl:+'
    
    new Date(movie.release_date).toLocaleDateString("ko", {
                                    month: "long",
                                    day: "numeric",
                                    year: "numeric",
                                })

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -44,6 +44,33 @@ const Votes = styled.Text`
     font-size: 10px;
 `;
 
+const ListContainer = styled.View`
+    margin-bottom: 40px;
+`;
+const HMovie = styled.View`
+    padding: 0px 30px;
+    margin-bottom: 30px;
+    flex-direction: row;
+`;
+const HColumn = styled.View`
+    margin-left: 15px;
+    width: 80%;
+`;
+const Overview = styled.Text`
+    color: white;
+    opacity: 0.8;
+    width: 80%;
+`;
+
+const Release = styled.Text`
+    color: white;
+    font-size: 12px;
+    margin-vertical: 10px;
+`;
+const ComingSoonTitle = styled(ListTitle)`
+    margin-bottom: 30px;
+`;
+
 // const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 const SCREEN_HEIGHT = Dimensions.get("window").height;
 
@@ -111,24 +138,48 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
 
                 ))}
             </Swiper>
-            <ListTitle>Trending Movies</ListTitle>
-            <TrendingScroll
-                contentContainerStyle={{ paddingLeft: 30 }}
-                horizontal
-                showsHorizontalScrollIndicator={false}
-            >
-                {trending.map((movie) => (
-                    <Movie key={movie.id}>
-                        <Poster path={movie.poster_path} />
-                        <Title>
-                            {movie.original_title ? movie.original_title.slice(0, 13) : null}
-                            {movie.original_title && movie.original_title.length > 13 ? "..." : null}
-                        </Title>
-                        <Votes>⭐️ {movie.vote_average}/10</Votes>
-                    </Movie>
+            <ListContainer>
+                <ListTitle>Trending Movies</ListTitle>
+                <TrendingScroll
+                    contentContainerStyle={{ paddingLeft: 30 }}
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                >
+                    {trending.map((movie) => (
+                        <Movie key={movie.id}>
+                            <Poster path={movie.poster_path} />
+                            <Title>
+                                {movie.original_title ? movie.original_title.slice(0, 13) : null}
+                                {movie.original_title && movie.original_title.length > 13 ? "..." : null}
+                            </Title>
+                            <Votes>{movie.vote_average > 0
+                                ? `⭐️ ${movie.vote_average}/10`
+                                : `Coming soon`}
+                            </Votes>
+                        </Movie>
 
-                ))}
-            </TrendingScroll>
+                    ))}
+                </TrendingScroll>
+            </ListContainer>
+            <ComingSoonTitle>Coming soon</ComingSoonTitle>
+            {upcoming.map((movie) => (
+                <HMovie key={movie.id}>
+                    <Poster path={movie.poster_path} />
+                    <HColumn>
+                        <Title>{movie.original_title}</Title>
+                        <Release>
+                            {new Date(movie.release_date).toLocaleDateString("ko", {
+                                month: "long",
+                                day: "numeric",
+                                year: "numeric",
+                            })}
+                        </Release>
+                        <Overview>{movie.overview !== "" && movie.overview.length > 80
+                            ? `${movie.overview.slice(0, 140)}...`
+                            : movie.overview}</Overview>
+                    </HColumn>
+                </HMovie>
+            ))}
 
         </Container>
     );
