011c27038474a4834e6ad1a30e49897d534d8ce1
hohyoung <gabozanet@gmail.com>
2022-06-14 21:17:03 +0900

    Trending Movies

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import { ActivityIndicator, Dimensions } from "react-native";
 import Swiper from 'react-native-swiper';
 import styled from "styled-components/native";
+import Poster from "../components/Poster";
 import Slide from "../components/Slide";
 
 const API_KEY = "5dce6d21a6dda399669ed4fb16a2ee80";
@@ -16,7 +17,32 @@ const Loader = styled.View`
     align-items: center;
 `;
 
+const ListTitle = styled.Text`
+    color: white;
+    font-size: 18px;
+    font-weight: 600;
+    margin-left: 30px;
+`;
 
+const TrendingScroll = styled.ScrollView`
+    margin-top: 20px;
+`;
+
+const Movie = styled.View`
+    margin-right: 20px;
+    align-items: center;
+`;
+
+const Title = styled.Text`
+    color: white;
+    font-weight: 600;
+    margin-top: 7px;
+    margin-bottom: 5px;
+`;
+const Votes = styled.Text`
+    color: rgba(255, 255, 255, 0.8);
+    font-size: 10px;
+`;
 
 // const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 const SCREEN_HEIGHT = Dimensions.get("window").height;
@@ -26,13 +52,13 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
     const [isLoading, setLoading] = useState(true);
     const [nowPlaying, setNowPlaying] = useState([]);
     const [upcoming, setUpcoming] = useState([]);
-    const [tranding, setTranding] = useState([]);
-    const getTranding = async () => {
+    const [trending, setTrending] = useState([]);
+    const getTrending = async () => {
         const { results } = await (
             await fetch(
-                `https://api.themoviedb.org/3/tranding/movie/week?api_key=${API_KEY}&language=en-US&page=1&region=KR`
+                `https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}`
             )).json();
-        setTranding(results);
+        setTrending(results);
     };
     const getUpComing = async () => {
         const { results } = await (
@@ -51,7 +77,7 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
 
     };
     const getData = async () => {
-        await Promise.all([getTranding(), getUpComing(), getNowPlaying()]);
+        await Promise.all([getTrending(), getUpComing(), getNowPlaying()]);
         setLoading(false);
     }
     useEffect(() => {
@@ -71,7 +97,7 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
                 autoplayTimeout={3.5}
                 showsPagination={false}
                 showsButtons={false}
-                containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4 }}
+                containerStyle={{ marginBottom: 30, width: "100%", height: SCREEN_HEIGHT / 4 }}
             >
                 {nowPlaying.map((movie) => (
                     <Slide
@@ -85,6 +111,24 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
 
                 ))}
             </Swiper>
+            <ListTitle>Trending Movies</ListTitle>
+            <TrendingScroll
+                contentContainerStyle={{ paddingLeft: 30 }}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+            >
+                {trending.map((movie) => (
+                    <Movie key={movie.id}>
+                        <Poster path={movie.poster_path} />
+                        <Title>
+                            {movie.original_title ? movie.original_title.slice(0, 13) : null}
+                            {movie.original_title && movie.original_title.length > 13 ? "..." : null}
+                        </Title>
+                        <Votes>⭐️ {movie.vote_average}/10</Votes>
+                    </Movie>
+
+                ))}
+            </TrendingScroll>
 
         </Container>
     );
