02effe4d0db2dd56e206f0db8b0a8aed64f0487f
na505050 <master@na50.xyz>
2022-06-19 15:27:17 +0900

    Search part Three

--- screens/Search.tsx
+++ screens/Search.tsx
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import { View, Text } from "react-native";
+import { useQuery } from "react-query";
 import styled from "styled-components/native";
+import { moviesApi, tvApi } from "../api";
+import HList from "../components/HList";
+import Loader from "../components/Loader";
 
 const Container = styled.ScrollView``;
 
@@ -10,12 +14,32 @@ const SearchBar = styled.TextInput`
     border-radius: 15px;
     width: 90%;
     margin: 10px auto;
+    margin-bottom: 40px;
 `;
-
 const Search = () => {
     const [query, setQuery] = useState("");
+    const {
+        isLoading: moviesLoading,
+        data: moviesData,
+        refetch: searchMovies,
+    } = useQuery(["searchMovies", query], moviesApi.search, {
+        enabled: false,
+    });
+    const {
+        isLoading: tvLoading,
+        data: tvData,
+        refetch: searchTv,
+    } = useQuery(["searchTv", query], tvApi.search, {
+        enabled: false,
+    });
     const onChangeText = (text: string) => setQuery(text);
-    console.log(query);
+    const onSubmit = () => {
+        if (query === "") {
+            return;
+        }
+        searchMovies();
+        searchTv();
+    };
     return (
         <Container>
             <SearchBar
@@ -23,7 +47,13 @@ const Search = () => {
                 placeholderTextColor="grey"
                 returnKeyType="search"
                 onChangeText={onChangeText}
+                onSubmitEditing={onSubmit}
             />
+            {moviesLoading || tvLoading ? <Loader /> : null}
+            {moviesData ? (
+                <HList title="Movie Results" data={moviesData.results} />
+            ) : null}
+            {tvData ? <HList title="TV Results" data={tvData.results} /> : null}
         </Container>
     );
 };
